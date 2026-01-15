113151c873bfc505b402ea66c0187bbeaa6a1e51
Niv Badichi <nivbad88@gmail.com>
2026-01-15 08:26:34 +0200

    Clean up Battleship.hpp file
    Removed unnecessary comments and cleaned up formatting.

--- Battleship.hpp
+++ Battleship.hpp
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-/*======================================
-created by Alex Tkachenkov & Niv Badichi
-  ======================================*/
 
-
-
-
-
-
-#pragma once
-#include "Ship.hpp"
-
-class Battleship : public Ship
-{
-public:
-    Battleship();
-    ~Battleship() override;
-};
-=======
 #pragma once
 #include "Ship.hpp"
 
