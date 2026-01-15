4edee52c8c71c5f6f0a52891db24efa0e4de2969
Niv Badichi <nivbad88@gmail.com>
2026-01-15 08:25:27 +0200

    Clean up AiPlayer.hpp by removing comments and duplicates
    Removed unnecessary comments and duplicate code.

--- AiPlayer.hpp
+++ AiPlayer.hpp
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-/*======================================
-created by Alex Tkachenkov & Niv Badichi
-  ======================================*/
 
-
-
-
-
-
-#pragma once
-#include "Player.hpp"
-
-class AiPlayer : public Player {
-public:
-    AiPlayer(const char* name);
-    ~AiPlayer() override;
-
-    void placeAllShips() override;
-    void makeMove(Player* opponent) override;
-private:
-    int getRandomcoordinate() const;
-    bool getRandomBoolean() const;
-=======
 #pragma once
 #include "Player.hpp"
 
