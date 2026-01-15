0be02ce52bcf8f0d1ba9456efd9a06da66205aed
Niv Badichi <nivbad88@gmail.com>
2026-01-15 08:31:12 +0200

    Clean up Carrier.cpp by removing comments
    Removed unnecessary comments and cleaned up code formatting.

--- Carrier.cpp
+++ Carrier.cpp
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
-/*======================================
-created by Alex Tkachenkov & Niv Badichi
-  ======================================*/
-
-
-
-
-
-
 #include "Carrier.hpp"
 
 Carrier::Carrier() : Ship("Carrier", 5) {
 }
 Carrier::~Carrier() {
-=======
-#include "Carrier.hpp"
-
-Carrier::Carrier() : Ship("Carrier", 5) {
-}
-Carrier::~Carrier() {
->>>>>>> origin/main
 }
