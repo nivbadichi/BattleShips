7518c3e59fa3140246582a0d11c8bdf02139b6df
Niv Badichi <nivbad88@gmail.com>
2026-01-15 08:26:14 +0200

    Clean up Battleship.cpp by removing comments
    Removed commented header and cleaned up formatting.

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-/*======================================
-created by Alex Tkachenkov & Niv Badichi
-  ======================================*/
 
-
-
-
-
-
-#include "Battleship.hpp"
-
-Battleship::Battleship() : Ship("Battleship", 4) {
-}
-Battleship::~Battleship() {
-=======
 #include "Battleship.hpp"
 
 Battleship::Battleship() : Ship("Battleship", 4) {
